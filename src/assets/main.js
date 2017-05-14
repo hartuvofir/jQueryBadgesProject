c05cc584495ad23c1045e6222432e561f001ce8b
Ofir Hartuv <hartuvofir@gmail.com>
2017-05-14 03:25:07 +0300

    s

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,7 +10,7 @@ $.ajax("https://www.codeschool.com/users/ofirhartuv.json", {
       let newDiv = `<div class="course">
         <h3>${course.title}</h3>
         <img src="${course.badge}"></img>
-        <a href="${course.url}" target="_blank">See Course</a>
+        <a class="btn btn-primary" href="${course.url}" target="_blank">See Course</a>
        </div>`;
 
         $('#badges').append($(newDiv));
