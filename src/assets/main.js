03fd67c79d20547c1a4b9490bd84f9d57842f075
Ofir Hartuv <hartuvofir@gmail.com>
2017-05-14 03:04:38 +0300

    first step

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
   // your code will go here
+$.ajax("https://www.codeschool.com/users/ofirhartuv.json", {
+ dataType : 'jsonp',
+ success: function(result){
+   $.each(response.courses.completed,function(index,course){
+      let newDiv = `<div class="course">
+        <h3 title="${course}"></h3>
+        <img src="${course.badge}"></img>
+        <a href="${course.url}" target="_blank">See Course</a>
+       </div>`;
 
+        $('#badges').append($(newDiv));
+
+   });
+
+ }
+
+})
 });
